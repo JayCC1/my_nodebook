88cb1d73e43a23ca2760254fe24a8de134fa9437
Jaycee <1093033394@qq.com>
2023-09-15 16:09:43 +0800

    build(gulp-test): gulp 项目初始化

---
+++ packages/gulp-test/build/gulpfile.ts
@@ -0,0 +1,15 @@
+// 打包方式：串行(series) 并行(parallel)
+import { series, parallel } from "gulp";
+import { withTaskName, run } from "./utils";
+// gulp 不叫打包，做代码转化 vite
+
+/**
+ * 1. 打包样式
+ * 2. 打包工具方法
+ * 3. 打包所有组件
+ * 4. 打包每个组件
+ * 5. 生成一个组件库
+ * 6. 发布组件
+ */
+
+export default series(withTaskName("clean", async () => run("rimraf ./dist"))); // 删除 dist 目录
---
+++ packages/gulp-test/build/utils/paths.ts
@@ -0,0 +1,7 @@
+import { resolve } from "path";
+
+// gulp 项目的根目录
+export const projectRoot = resolve(__dirname, "../../");
+
+// gulp 打包输出目录
+export const outDir = resolve(__dirname, "../../dist");
