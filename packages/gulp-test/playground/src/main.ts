c29859fa51a9a4c1754278d3cf57b653ef0b0fd3
Jaycee <1093033394@qq.com>
2023-10-16 17:27:59 +0800

    feat(gulp-test): 测试自定义ui库的按需导入插件功能

--- packages/gulp-test/playground/src/main.ts
+++ packages/gulp-test/playground/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./app.vue";
-import * as comp from "@jaycce/gulp-components";
-import "@jaycce/gulp-components/es/components/button/style/index";
+// 全局引入方式
+// import * as comp from "@jaycce/gulp-components";
+// import "@jaycce/gulp-components/es/components/button/style/index";
+// const app = createApp(App);
+// app.use(comp);
 
+// 自动按需引入测试
 const app = createApp(App);
-
-app.use(comp);
 app.mount("#app");
--- packages/gulp-test/playground/vite.config.ts
+++ packages/gulp-test/playground/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { uiResolver } from "@jaycce/gulp-utils/lib/ui-resolvers";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [uiResolver()],
+    }),
+  ],
 });
